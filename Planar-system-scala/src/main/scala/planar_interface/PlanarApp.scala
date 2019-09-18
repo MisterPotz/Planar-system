7f9623f512fe338cd9378a995aa87e38d9b25837
Alexander Gornostaev <aleksgornostaev@gmail.com>
2019-09-18 08:04:01 +0300

    Scala files/ FXML added

---
+++ Planar-system-scala/src/main/scala/planar_interface/PlanarApp.scala
@@ -0,0 +1,20 @@
+package planar_interface
+
+import scalafx.application.JFXApp
+import scalafx.application.JFXApp.PrimaryStage
+import scalafx.geometry.Insets
+import scalafx.scene.Scene
+import scalafx.scene.control.Label
+import scalafx.scene.layout.BorderPane
+
+object PlanarApp extends JFXApp {
+  stage = new PrimaryStage {
+    title = "Hello"
+    scene = new Scene {
+      root = new BorderPane {
+        padding = Insets(25)
+        center = new Label("Hello World")
+      }
+    }
+  }
+}
